912f1f894cc7a021874e156c0f93b464417df81f
2talladam <2talladam@gmail.com>
2024-10-01 21:14:35 -0600

    Header.tsx, navbar.tsx

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect} from 'react';
 import { Link} from 'react-router-dom';
 import auth from '';
-
+import Header from './Header.tsx';
 const Navbar = () => {
 
     const [loginCheck, setLoginCheck] = useState(false);
@@ -18,7 +18,42 @@ const Navbar = () => {
 
     const checkSignedup = () => {
         if() {
-            
+            if (auth.isSignedUp()) { 
+                setSignupCheck(true);
+            } else {
+                setSignupCheck(false);
+            }
+        };
+    
         }
     }
-}
+    return(
+        <nav>
+        <ul>
+            <li>
+                <Link to="/Home">Home</Link>
+            </li>
+            {loginCheck ? (
+                <>
+                    <li>
+                        <Link to="/Saved-Recipes">Saved Recipes</Link>
+                    </li>
+                </>
+            ) : (
+                <>
+                    <li>
+                        <Link to="/Contact-us">Contact Us</Link>
+                    </li>
+                </>
+            )}
+            {!signupCheck && (
+                <li>
+                    <Link to="/Contact-us">Contact Us</Link>
+                </li>
+            )}
+        </ul>
+    </nav>
+);
+
+
+export default Navbar;
