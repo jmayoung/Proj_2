1e2accd0a58b968e4fdfcbd56c93701db95b6e58
2talladam <2talladam@gmail.com>
2024-10-05 16:04:20 -0600

    Added body

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,21 +1,16 @@
 import React, { useState } from 'react';
 
-interface Recipe {
-  id: number;
-  title: string;
-}
-
 const App = () => {
   const [ingredients, setIngredients] = useState('');
-  const [recipes, setRecipes] = useState<Recipe[]>([]);
+  const [recipes, setRecipes] = useState([]);
 
   const fetchRecipes = async () => {
-    const response = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredients}&apiKey=512a655e48f54350ab4a1694077b9966`);
+    const response = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredients}&apiKey=YOUR_API_KEY`);
     const data = await response.json();
     setRecipes(data);
   };
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     fetchRecipes();
   };
@@ -42,5 +37,4 @@ const App = () => {
     </div>
   );
 };
-
 export default App;
